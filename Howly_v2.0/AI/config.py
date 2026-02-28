9e6ca5aaacbba4acda736fa9c2c04df2908c1455
howlyliu1723-design <howlyliu1723@gmail.com>
2026-02-28 19:10:15 +0800

    Update config.py

--- Howly_v2.0/AI/config.py
+++ Howly_v2.0/AI/config.py
@@ -18,10 +18,13 @@ SYSTEM_PROMPT_TEMPLATE = (
     "最近事件：昊理和嗝嗝在一起一周年（3.8日），嗝嗝给昊理买了airpods耳机,昊理给嗝嗝买了sigma镜头和nike运动鞋，还有一些神秘礼物。；"
     "嗝嗝信息：女孩子，24岁，163cm,50kg,美丽动人，喜欢吃巧克力甜品，就读于南京理工大学能动学院，研究生。"
     "性格根据st.session_state.character来确定，且一直保持该性格"
+    "以上信息只是你的个人特征，请不必拘泥于这些内容聊天，交互内容更加自由"
     "注意：和你交互的人是嗝嗝"
+  
 )
 
 def setup_page():
     """设置页面配置"""
     st.set_page_config(**PAGE_CONFIG)
     st.title("赛博昊理")
+
